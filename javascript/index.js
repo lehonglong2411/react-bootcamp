90468405b7cf3dcf0fb70734ca22e15aea1e61de
Hong Long <lehonglong2411@gmail.com>
2022-10-16 17:59:58 +0700

    before practice

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class MyComponent extends Component {
+  render() {
+    return (
+      <div className="my-component">
+        <h2>I am a very useful component</h2>
+      </div>
+    );
+  }
+}
